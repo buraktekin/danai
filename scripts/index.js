e6ae2c550d9637d74c13b64ee8ec4b6dd0db3655
buraktekin <tknbrk@gmail.com>
2021-12-05 12:04:47 +0100

    happy bday DANAI

--- scripts/index.js
+++ scripts/index.js
@@ -45,6 +45,7 @@ function schoolPride() {
 
     if (Date.now() < end) {
       requestAnimationFrame(frame);
+      document.getElementById("item2").scrollLeft += 1;
     }
   }());
 }
@@ -58,7 +59,7 @@ function handleTickInit(tick) {
   var timeDuration = Tick.helper.duration(24, 'hours');
   // add 24 hours to get final deadline
   var t = new Date();
-  var deadline = new Date('December 05, 2021 00:00:00');
+  var deadline = new Date(t.setSeconds(t.getSeconds() + 1));
   // create counter
   var counter = Tick.count.down(deadline, { format: ['h', 'm', 's'] });
 
